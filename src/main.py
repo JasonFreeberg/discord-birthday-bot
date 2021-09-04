75f95797c815b06b0aff6cff8251598f208f0a06
Jason Freeberg <jafreebe@microsoft.com>
2021-09-03 21:50:43 -0700

    Better integration tests

--- src/main.py
+++ src/main.py
@@ -14,13 +14,23 @@ def main(friends_file_path: str):
 
     print('Runing application...')
     
-    BOT_TOKEN = 'Bot '+os.environ.get('BOT_TOKEN')
-    CHANNEL_ID = os.environ.get('SERVER_ID')
+    BOT_TOKEN = os.environ.get('BOT_TOKEN')
+    CHANNEL_ID = os.environ.get('CHANNEL_ID')
+
+    if BOT_TOKEN is None:
+        raise ValueError('No env var found for BOT_TOKEN')
+    if CHANNEL_ID is None:
+        raise ValueError('No env var found for CHANNEL_ID')
+
+    BOT_TOKEN = 'Bot '+BOT_TOKEN
     BASE_URL = 'https://discord.com/api/v9'
     TODAY = datetime.datetime.today()
 
     with open(friends_file_path) as json_file:
         birthday_data = json.load(json_file)
+
+        if len(birthday_data['friends']) < 1: 
+            raise ValueError('No friend objects found in the list of friends. There was likely a problem reading the file.')
 
         for friend_entry in birthday_data['friends']:
             friend = Friend.from_dict(friend_entry)
--- test/test_e2e.py
+++ test/test_e2e.py
@@ -1,3 +1,7 @@
+import json
+import os
+from os.path import join
+from datetime import date, datetime, timedelta
 from freezegun import freeze_time
 from src import main
 import responses
@@ -7,16 +11,41 @@ BOT_TOKEN = 'Bot aaabbbcccdddeeefff'
 FROZEN_TIME = '2021-07-01'
 
 
-@responses.activate
 def test_e2e(monkeypatch):
 
-    responses.add(responses.POST, f'https://discord.com/api/v9/channels/{CHANNEL_ID}/messages', json={
-                  'error': 'not found'}, status=404)
-    monkeypatch.setenv('CHANNEL_ID', CHANNEL_ID)
-    monkeypatch.setenv('BOT_TOKEN', BOT_TOKEN)
+    DAYS_UNTIL_BIRTHDAY = 14
+    EXPECTED_MESSAGE='Joe Integration Testerson is turning 0 in two weeks on September 18. Maybe we should get him A new keyboard ?'
+    TEST_DATA_FILE = join(os.getcwd(),'temp','test-data.json')
+    BIRTHDAY = date.today() + timedelta(days=DAYS_UNTIL_BIRTHDAY+1)
 
-    with freeze_time(FROZEN_TIME):
-        main.main('test/test_data.json')
+    test_data = {
+        "date_format": "%Y-%m-%d",
+        "reminder_in_days": str(DAYS_UNTIL_BIRTHDAY),
+        "friends": [
+            {
+                "name": "Joe Integration Testerson",
+                "birthdate": BIRTHDAY.strftime("%Y-%m-%d"),
+                "gift_ideas": [
+                    "A new keyboard"
+                ]
+            }
+        ]
+    }
 
-        assert len(responses.calls) == 2
-        assert responses.calls[0].body.content == ''
+    # Create directory for test data (https://stackoverflow.com/questions/12517451/automatically-creating-directories-with-file-output)
+    if not os.path.exists(os.path.dirname(TEST_DATA_FILE)):
+        try:
+            os.makedirs(os.path.dirname(TEST_DATA_FILE))
+        except OSError as exc: # Guard against race condition
+            if exc.errno != errno.EEXIST:
+                raise
+
+    # Write test file and run script
+    with open(TEST_DATA_FILE, 'w') as file:
+        json.dump(test_data, file, indent=4)
+
+    main.main(friends_file_path=TEST_DATA_FILE)
+
+    """
+    ToDo: Call discord API to check if the new message is there
+    """
